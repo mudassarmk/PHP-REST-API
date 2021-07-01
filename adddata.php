037da6a1f8697c32c987543177a4de7ce7047ae2
mudassarmk <80820363+mudassarmk@users.noreply.github.com>
2021-07-01 13:57:14 -0700

    Update adddata.php

--- adddata.php
+++ adddata.php
@@ -1,12 +1,9 @@
 <?php
 
-	include 'conn.php';
-	
-	$itemcode = $_POST['itemcode'];
-	$itemname = $_POST['itemname'];
-	$price = $_POST['price'];
-	$stock= $_POST['stock'];
-	
-	$connect->query("INSERT INTO tb_item (item_code,item_name,price,stock) VALUES ('".$itemcode."','".$itemname."','".$price."','".$stock."')")
+$connect = new mysqli("localhost","root","","my_store");
 
-?>
+if($connect){
+	 
+}else{
+	echo "Connection Failed";
+	exit();
